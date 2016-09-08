ce0f0c6ea13cc35a4026426c55d5f791f785eb1a
Michael Cruz <michaelcruz@aim.com>
2016-09-08 11:28:27 -0400

    next up

--- SwiftApp/ViewController.swift
+++ SwiftApp/ViewController.swift
@@ -23,8 +23,8 @@ class ViewController: UIViewController {
     
     @IBAction func buttonTapped(_ sender: AnyObject) {
         
-        print(text1.text!)
-        print(text2.text!)
+        theLabel.text = "\(Double(text1.text!)! + Double(text2.text!)!)"
+        
     }
     
     override func viewDidLoad() {
